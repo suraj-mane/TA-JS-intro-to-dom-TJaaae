068c108490927a93b8f3cf883a8d08c8bc3ac896
suraj-mane <sun.mane1100@gmail.com>
2022-01-22 01:49:17 +0530

    assignment is complete

--- block-BJaaef/code/script.js
+++ block-BJaaef/code/script.js
@@ -1,1 +1,29 @@
-got.houses.forEach(())
+console.log(got);
+
+let parentElm  = document.querySelector(".mainbox");
+
+let allpeople = got.houses.reduce((acc, cv) => {
+  acc = acc.concat(cv.people);
+  return acc;
+}, []);
+
+ let cardHTML = allpeople.map((person) => {
+  return `<li class="card">
+    <div class="info">
+    <img 
+      src = ${person.image}
+      alt = ${person.name}
+    />
+    <h2>${person.name}</h2>
+    <div>
+    <p>
+      ${person.description}
+    </p>
+    <a href =${person.wikilink}> Learn More!</a>
+    </li>`;
+});
+
+parentElm.innerHTML = cardHTML;
+
+console.log(allpeople);
+console.log(cardHTML);
